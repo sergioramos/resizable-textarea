e62c8ee0ec98b6742b2edf2a9942e63a59041f34
Sérgio Ramos <mail@sergioramos.me>
2012-12-02 03:30:35 +0000

    0.0.2
    * less code, working **way** better

--- rta.js
+++ rta.js
@@ -2,19 +2,10 @@ var ev = require('event');
 
 module.exports = function (element) {
   ev.bind(element, 'input', function () {
-    var initialScroll = this.scrollWidth;
-    
-    this.setAttribute('wrap', 'off')
-    var scroll = this.scrollWidth;
-    this.setAttribute('wrap', '');
-    
-    var rows = Math.floor((scroll/initialScroll)%initialScroll);
-    if(rows > 1) this.setAttribute('rows', rows + 1);
     rows = parseInt(this.getAttribute('rows'));
-    
+    var scrollHeight = this.scrollHeight;
     var height = this.clientHeight;
-    scroll = this.scrollHeight;
     
-    if(scroll > height) this.setAttribute('rows', rows + 1);
+    this.setAttribute('rows', Math.ceil(((scrollHeight * rows) / height)))
   });
 };
